19a57b0b9e4a23ba8444491c98ceb0db79973baf
Emilio Oscar <144294514+EmilioOscar@users.noreply.github.com>
2026-09-25 11:15:53 +0100

    Fix comment formatting in ProfilerUI.h

--- ProfilerUI.h
+++ ProfilerUI.h
@@ -2,11 +2,10 @@
 
 namespace LightweightProfiler {
 
-    // Handles all Dear ImGui and ImPlot rendering logic safely separated from the game loop
+    // Handles all Dear ImGui and ImPlot rendering logic  separated from the game loop
     class Dashboard {
     public:
         // Renders all profiler windows. 
-        //  (will add the 10Hz throttle inside this function next)
         static void Draw();
     };
 
